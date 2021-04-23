28f63c1d0e4fbab7de88738aa5bccc711b071647
Daria Dziubałtowska <daria305@gmail.com>
2021-04-23 23:51:15 +0200

    Add get question by id

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
 import users from "./users.js";
+import questions from "./questions.js";
 
 export default (app) => {
     app.use("/api/users", users);
+    app.use("/api/questions", questions);
 };
