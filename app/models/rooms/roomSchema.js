66697612dcac8e542a6d69672c970a750088e28d
Daria Dziubałtowska <daria305@gmail.com>
2021-04-23 22:28:49 +0200

    Add schemas

---
+++ app/models/rooms/roomSchema.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            lowercase: true,
+        },
+
+        ownerId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        questionCollections: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Collection",
+            required: true,
+        },
+
+        questionsAsked: {
+            type: [
+                {
+                    id: mongoose.Schema.Types.ObjectId,
+                    askedAt: Date,
+                },
+            ],
+            ref: "Question",
+            required: true,
+        },
+
+        guests: {
+            type: [
+                {
+                    email: String,
+                    answers: [
+                        {
+                            questionId: {
+                                type: mongoose.Schema.Types.ObjectId,
+                                ref: "Question",
+                            },
+                            chosenAnswer: Number,
+                            answeredAt: Date,
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    { timestamps: true },
+);
+
+export default roomSchema;
